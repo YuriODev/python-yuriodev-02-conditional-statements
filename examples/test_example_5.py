cf57faade39973729768e481b2bea10680cce4c5
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-14 17:06:36 +0000

    Update test_example_5.py

--- examples/test_example_5.py
+++ examples/test_example_5.py
@@ -1,22 +1,28 @@
 import unittest
 import subprocess
+import os
+
+# This is a general approach; you'll need to define exercise_file_path for each specific script you're testing
+exercise_file_path = os.path.join(os.path.dirname(__file__), "example_5.py")  # Adjust "exercise_1.py" accordingly
 
 
-class TestExample5(unittest.TestCase):
-    def test_output_6785(self):
-        # Test when the input is 6785
-        output = subprocess.check_output(['python3', 'example_5.py'], input='6785\n', text=True)
-        self.assertEqual(output, 'Enter the number of seconds: 0 day(s), 1 hour(s), 53 minute(s), 5 second(s)\n')
+class TestDayOfWeek(unittest.TestCase):
+    def run_exercise(self, input_value):
+        """Helper method to run the exercise script with the provided input and return its output."""
+        return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
 
-    def test_output_456789(self):
-        # Test when the input is 456789
-        output = subprocess.check_output(['python3', 'example_5.py'], input='456789\n', text=True)
-        self.assertEqual(output, 'Enter the number of seconds: 5 day(s), 6 hour(s), 53 minute(s), 9 second(s)\n')
+    def test_grade_a(self):
+        output = self.run_exercise("95\n")
+        self.assertIn("Your grade is A.", output)
 
-    def test_output_86401(self):
-        # Test when the input is 86401
-        output = subprocess.check_output(['python3', 'example_5.py'], input='86401\n', text=True)
-        self.assertEqual(output, 'Enter the number of seconds: 1 day(s), 0 hour(s), 0 minute(s), 1 second(s)\n')
+    def test_grade_c(self):
+        output = self.run_exercise("74\n")
+        self.assertIn("Your grade is C.", output)
+
+    def test_grade_d(self):
+        output = self.run_exercise("60\n")
+        self.assertIn("Your grade is D.", output)
+
 
 
 if __name__ == '__main__':
