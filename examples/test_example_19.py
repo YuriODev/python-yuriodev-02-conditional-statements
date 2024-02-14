4a75ef78dee15389805550259423ee9b22c568ec
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-14 17:07:57 +0000

    Update test_example_19.py

--- examples/test_example_19.py
+++ examples/test_example_19.py
@@ -1,23 +1,23 @@
 import unittest
 import subprocess
+import os
 
+class TestTriangleTypeDetermination(unittest.TestCase):
+    def run_exercise(self, input_value):
+        exercise_file_path = os.path.join(os.path.dirname(__file__), "example_19.py")
+        return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
 
-class TestExample9(unittest.TestCase):
-    def test_output_100(self):
-        # Test when the input is 100
-        output = subprocess.check_output(['python3', 'example_9.py'], input='100\n', text=True)
-        self.assertEqual(output, 'Enter the number of natural numbers to sum: 5050\n')
+    def test_rectangular(self):
+        output = self.run_exercise("3\n4\n5\n")
+        self.assertIn("rectangular", output)
 
-    def test_output_15(self):
-        # Test when the input is 15
-        output = subprocess.check_output(['python3', 'example_9.py'], input='15\n', text=True)
-        self.assertEqual(output, 'Enter the number of natural numbers to sum: 120\n')
+    def test_acute(self):
+        output = self.run_exercise("3\n3\n4\n")
+        self.assertIn("acute", output)
 
-    def test_output_99(self):
-        # Test when the input is 99
-        output = subprocess.check_output(['python3', 'example_9.py'], input='99\n', text=True)
-        self.assertEqual(output, 'Enter the number of natural numbers to sum: 4950\n')
-
+    def test_obtuse(self):
+        output = self.run_exercise("3\n4\n6\n")
+        self.assertIn("obtuse", output)
 
 if __name__ == '__main__':
     unittest.main()
