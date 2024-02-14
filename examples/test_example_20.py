3cc7970a4ee714902566004310c11e726ea8b078
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-14 17:08:01 +0000

    Update test_example_20.py

--- examples/test_example_20.py
+++ examples/test_example_20.py
@@ -1,23 +1,23 @@
 import unittest
 import subprocess
+import os
 
+class TestIdenticalDigitsCounting(unittest.TestCase):
+    def run_exercise(self, input_value):
+        exercise_file_path = os.path.join(os.path.dirname(__file__), "example_20.py")
+        return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
 
-class TestExample10(unittest.TestCase):
-    def test_output_184(self):
-        # Test when the input is 184
-        output = subprocess.check_output(['python3', 'example_10.py'], input='184\n', text=True)
-        self.assertEqual(output, 'Enter a three-digit number: 481\n')
+    def test_three_identical_digits(self):
+        output = self.run_exercise("777\n")
+        self.assertIn("3", output)
 
-    def test_output_123(self):
-        # Test when the input is 123
-        output = subprocess.check_output(['python3', 'example_10.py'], input='123\n', text=True)
-        self.assertEqual(output, 'Enter a three-digit number: 321\n')
+    def test_two_identical_digits(self):
+        output = self.run_exercise("727\n")
+        self.assertIn("2", output)
 
-    def test_output_567(self):
-        # Test when the input is 567
-        output = subprocess.check_output(['python3', 'example_10.py'], input='567\n', text=True)
-        self.assertEqual(output, 'Enter a three-digit number: 765\n')
-
+    def test_no_identical_digits(self):
+        output = self.run_exercise("123\n")
+        self.assertIn("0", output)
 
 if __name__ == '__main__':
     unittest.main()
