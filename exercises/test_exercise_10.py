cacc8f17aed6499aa855d98ef28b65154d754c09
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-15 19:41:38 +0000

    Update test_exercise_10.py

--- exercises/test_exercise_10.py
+++ exercises/test_exercise_10.py
@@ -2,29 +2,23 @@ import unittest
 import subprocess
 import os
 
-# Define the full path to the exercise script
-exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_10.py")
 
+class TestExercise10(unittest.TestCase):
 
-class TestMinuteHandAngle(unittest.TestCase):
     def run_exercise(self, input_value):
         """Helper method to run the exercise script with the provided input and return its output."""
+        exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_10.py")
         return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
 
-    def test_example_1(self):
-        """Test the provided example 1."""
-        output = self.run_exercise("190\n")
-        self.assertEqual(output, "120.0\n")
+    def test_right_angled_triangle(self):
+        """Triangle is right-angled."""
+        output = self.run_exercise("0\n0\n0\n2\n4\n0\n")
+        self.assertIn("Yes", output)
 
-    def test_example_2(self):
-        """Test the provided example 2."""
-        output = self.run_exercise("170\n")
-        self.assertEqual(output, "240.0\n")
-
-    def test_example_3(self):
-        """Test the provided example 3."""
-        output = self.run_exercise("370\n")
-        self.assertEqual(output, "120.0\n")
+    def test_not_right_angled_triangle(self):
+        """Triangle is not right-angled."""
+        output = self.run_exercise("0\n0\n0\n0\n0\n0\n")
+        self.assertIn("No", output)
 
 
 if __name__ == '__main__':
