43b10bea115946093ffb66ba366bbc007f62f7e3
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-15 19:41:35 +0000

    Update test_exercise_1.py

--- exercises/test_exercise_1.py
+++ exercises/test_exercise_1.py
@@ -2,34 +2,27 @@ import unittest
 import subprocess
 import os
 
-# Define the full path to the exercise script
-exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_1.py")
 
-
-class TestExample1(unittest.TestCase):
+class TestExercise1(unittest.TestCase):
     def run_exercise(self, input_value):
         """Helper method to run the exercise script with the provided input and return its output."""
+        exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_1.py")
         return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
 
-    def test_example_1(self):
-        """Test the provided example 1."""
-        output = self.run_exercise("12345\n")
-        self.assertIn("96\n", output)
+    def test_older_alex(self):
+        """Alex is older than Tatyana."""
+        output = self.run_exercise("30\n25\n")
+        self.assertIn("Alex is the eldest.", output)
 
-    def test_example_2(self):
-        """Test the provided example 2."""
-        output = self.run_exercise("44444\n")
-        self.assertIn("128\n", output)
+    def test_older_tatyana(self):
+        """Tatyana is older than Alex."""
+        output = self.run_exercise("22\n30\n")
+        self.assertIn("Tatyana is the eldest.", output)
 
-    def test_example_3(self):
-        """Test the provided example 3."""
-        output = self.run_exercise("11111\n")
-        self.assertIn("32\n", output)
-
-    def test_example_4(self):
-        """Test the provided example 4."""
-        output = self.run_exercise("12346\n")
-        self.assertIn("106\n", output)
+    def test_same_age(self):
+        """Alex and Tatyana are of the same age."""
+        output = self.run_exercise("25\n25\n")
+        self.assertIn("Alex and Tatyana are of the same age.", output)
 
 
 if __name__ == '__main__':
