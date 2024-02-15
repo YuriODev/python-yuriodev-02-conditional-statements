2a0deb88495ad93def6504f768e032428f4a8a78
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-15 19:41:40 +0000

    Update test_exercise_11.py

--- exercises/test_exercise_11.py
+++ exercises/test_exercise_11.py
@@ -2,39 +2,33 @@ import unittest
 import subprocess
 import os
 
-# Define the full path to the exercise script
-exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_11.py")
 
+class TestExercise11(unittest.TestCase):
 
-class TestBillDenominations(unittest.TestCase):
     def run_exercise(self, input_value):
         """Helper method to run the exercise script with the provided input and return its output."""
+        exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_11.py")
         return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
 
-    def test_example_1(self):
-        """Test the provided example 1."""
-        output = self.run_exercise("534\n")
-        self.assertEqual(output, "1 (500), 0 (100), 3 (10), 0 (5), 4 (1)\n")
+    def test_leap_year(self):
+        """Year is a leap year."""
+        output = self.run_exercise("2020\n")
+        self.assertIn("Leap year.", output)
 
-    def test_example_2(self):
-        """Test the provided example 2."""
-        output = self.run_exercise("1245\n")
-        self.assertEqual(output, "2 (500), 2 (100), 4 (10), 1 (5), 0 (1)\n")
+    def test_leap_year_2(self):
+        """Year is a leap year."""
+        output = self.run_exercise("2000\n")
+        self.assertIn("Leap year.", output)
 
-    def test_example_3(self):
-        """Test the provided example 3."""
-        output = self.run_exercise("12\n")
-        self.assertEqual(output, "0 (500), 0 (100), 1 (10), 0 (5), 2 (1)\n")
+    def test_leap_year_3(self):
+        """Year is a leap year."""
+        output = self.run_exercise("2016\n")
+        self.assertIn("Leap year.", output)
 
-    def test_example_4(self):
-        """Test the provided example 4."""
-        output = self.run_exercise("786\n")
-        self.assertEqual(output, "1 (500), 2 (100), 8 (10), 1 (5), 1 (1)\n")
-
-    def test_example_5(self):
-        """Test the provided example 5."""
-        output = self.run_exercise("377\n")
-        self.assertEqual(output, "0 (500), 3 (100), 7 (10), 1 (5), 2 (1)\n")
+    def test_not_leap_year(self):
+        """Year is not a leap year."""
+        output = self.run_exercise("2019\n")
+        self.assertIn("Ordinary year.", output)
 
 
 
