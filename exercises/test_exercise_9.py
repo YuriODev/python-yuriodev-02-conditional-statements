2901852239e7236521af74d8949ea4ded6c6f0d7
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-15 19:46:08 +0000

    Update test_exercise_9.py

--- exercises/test_exercise_9.py
+++ exercises/test_exercise_9.py
@@ -2,29 +2,28 @@ import unittest
 import subprocess
 import os
 
-# Define the full path to the exercise script
-exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_9.py")
 
+class TestExercise9(unittest.TestCase):
 
-class TesetHourAndAngle(unittest.TestCase):
     def run_exercise(self, input_value):
         """Helper method to run the exercise script with the provided input and return its output."""
+        exercise_file_path = os.path.join(os.path.dirname(__file__), "exercise_9.py")
         return subprocess.check_output(['python3', exercise_file_path], input=input_value, text=True, universal_newlines=True)
 
-    def test_example_1(self):
-        """Test the provided example 1."""
-        output = self.run_exercise("2\n30\n0\n")
-        self.assertEqual(output, "75.0\n")
+    def test_sum_greater(self):
+        """Sum of the first and last digits is greater than the second."""
+        output = self.run_exercise("531\n")
+        self.assertIn(">", output)
 
-    def test_example_2(self):
-        """Test the provided example 2."""
-        output = self.run_exercise("10\n0\n0\n")
-        self.assertEqual(output, "300.0\n")
+    def test_sum_lesser(self):
+        """Sum of the first and last digits is less than the second."""
+        output = self.run_exercise("254\n")
+        self.assertIn("<", output)
 
-    def test_example_3(self):
-        """Test the provided example 3."""
-        output = self.run_exercise("6\n0\n0\n")
-        self.assertEqual(output, "180.0\n")
+    def test_sum_equal(self):
+        """Sum of the first and last digits is equal to the second."""
+        output = self.run_exercise("423\n")
+        self.assertIn("=", output)
 
 
 if __name__ == '__main__':
