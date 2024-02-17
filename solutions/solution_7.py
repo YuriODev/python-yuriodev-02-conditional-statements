cc87f99109269b4636854ea2f2cb3f94bf5b17b2
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-17 17:50:41 +0000

    Update solution_7.py

--- solutions/solution_7.py
+++ solutions/solution_7.py
@@ -1,2 +1,31 @@
-# Your solution to Exercise 7
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /, mod, pow, div): ")
 
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Division by 0!"
+elif operation == "mod":
+    if num2 != 0:
+        result = num1 % num2
+    else:
+        result = "Division by 0!"
+elif operation == "pow":
+    result = num1 ** num2
+elif operation == "div":
+    if num2 != 0:
+        result = num1 // num2
+    else:
+        result = "Division by 0!"
+else:
+    result = "Invalid operation"
+
+print(result)
