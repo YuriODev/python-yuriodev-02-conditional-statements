d36187ff87cd2c029e4404e42c40da4dbe2fa967
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-17 18:01:24 +0000

    Update Solutions

--- solutions/solution_2.py
+++ solutions/solution_2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 age = int(input("Enter your age: "))
 
 if age <= 1:
@@ -9,7 +8,3 @@ elif age >= 13 and age < 20:
     print("You are a teenager.")
 else:
     print("You are an adult.")
-=======
-# Your solution to Exercise 2
-
->>>>>>> e2be20ee77cdbae9a6d1e5effe3c3653928557aa
