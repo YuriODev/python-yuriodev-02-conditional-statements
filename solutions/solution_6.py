0ec7564d81fcd2dff8b9aae8729dcc8cba2c6954
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-17 17:50:39 +0000

    Update solution_6.py

--- solutions/solution_6.py
+++ solutions/solution_6.py
@@ -1,2 +1,14 @@
 # Your solution to Exercise 6
 
+x1, y1 = int(input("Enter x1: ")), int(input("Enter y1: "))
+x2, y2 = int(input("Enter x2: ")), int(input("Enter y2: "))
+
+distance_a = x1**2 + y1**2
+distance_b = x2**2 + y2**2
+
+if distance_a > distance_b:
+    print("A is further from the origin.")
+elif distance_a < distance_b:
+    print("B is further from the origin.")
+else:
+    print("A and B are at the same distance from the origin.")
