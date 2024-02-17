cce3e46f0e45ba2de69ad85a0d661d3375f9d4f8
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-17 17:47:37 +0000

    Update solution_3.py

--- solutions/solution_3.py
+++ solutions/solution_3.py
@@ -1,2 +1,12 @@
-# Your solution to Exercise 3
+number = int(input("Enter a number (0-36): "))
 
+output = "The bet will play!"
+
+if number == 0:
+    output = "Green"
+elif 1 <= number <= 10 or 19 <= number <= 28:
+    output = "Black" if number % 2 else "Red"
+elif 11 <= number <= 18 or 29 <= number <= 36:
+    output = "Red" if number % 2 else "Black"
+
+print(output)
