909649a92635c4a6de8e0d825f38c30fb01ef5d8
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-17 17:50:45 +0000

    Update solution_8.py

--- solutions/solution_8.py
+++ solutions/solution_8.py
@@ -1,2 +1,6 @@
-# Your solution to Exercise 8
+number = int(input("Enter a three-digit number: "))
+digit = int(input("Enter a digit: "))
 
+is_in_number = digit == number % 10 or digit == (number // 10) % 10 or digit == number // 100
+
+print(is_in_number)
