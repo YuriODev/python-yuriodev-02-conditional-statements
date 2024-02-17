20bf959f7ccfaa567570c1307a108fde66d2c2c3
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-17 17:05:27 +0000

    Upload Exercise files

--- exercises/exercise_9.py
+++ exercises/exercise_9.py
@@ -1,15 +1,2 @@
+# Your solution to Exercise 9
 
-number = int(input("Enter a three-digit number: "))
-
-first_digit = number // 100
-second_digit = (number // 10) % 10
-third_digit = number % 10
-
-sum_comparison = first_digit + third_digit
-
-if sum_comparison > second_digit:
-    print(">")
-elif sum_comparison < second_digit:
-    print("<")
-else:
-    print("=")
