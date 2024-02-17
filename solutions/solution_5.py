07c1200d4cb9351d6c5ec6cbddb4de241871da3b
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-17 17:47:33 +0000

    Update solution_5.py

--- solutions/solution_5.py
+++ solutions/solution_5.py
@@ -1,2 +1,28 @@
-# Your solution to Exercise 5
+a = float(input("Enter coefficient a: "))
+b = float(input("Enter coefficient b: "))
+c = float(input("Enter coefficient c: "))
 
+output = "No roots."
+
+if a == 0:
+    if b != 0:
+        output = f"{-c / b}"
+elif b == 0:
+    if c != 0:
+        if -c / a >= 0:
+            x1 = (-c / a) ** 0.5
+            x2 = -(-c / a) ** 0.5
+            output = f"{x1} and {x2}"
+    else:
+        output = "0"
+else:
+    d = b ** 2 - 4 * a * c
+    if d > 0:
+        x1 = (-b + d ** 0.5) / (2 * a)
+        x2 = (-b - d ** 0.5) / (2 * a)
+        output = f"{x1} and {x2}"
+    elif d == 0:
+        x = -b / (2 * a)
+        output = f"{x}"
+
+print(output)
