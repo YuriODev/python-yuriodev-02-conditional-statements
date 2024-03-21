dc827ce7e176757a4ca6a0cffa53a8468c43a029
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-21 14:30:54 +0000

    Update solution_9.py

--- solutions/solution_9.py
+++ solutions/solution_9.py
@@ -1,12 +1,15 @@
-
+# Prompt the user to enter a three-digit number
 number = int(input("Enter a three-digit number: "))
 
+# Extract the first, second, and third digits of the number
 first_digit = number // 100
 second_digit = (number // 10) % 10
 third_digit = number % 10
 
+# Calculate the sum of the first and third digits
 sum_comparison = first_digit + third_digit
 
+# Compare the sum with the second digit and print the result
 if sum_comparison > second_digit:
     print(">")
 elif sum_comparison < second_digit:
