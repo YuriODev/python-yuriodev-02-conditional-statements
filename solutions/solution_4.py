a7805a3fb8f07e85a0db6e4c6ab23091683ec04f
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-21 14:05:15 +0000

    Update solution_4.py

--- solutions/solution_4.py
+++ solutions/solution_4.py
@@ -1,12 +1,22 @@
+# Prompt the user to enter a grade and convert it to an integer
 grade = int(input("Enter the school grade (1-12): "))
 
+# Check the grade and print the corresponding level
 if 1 <= grade <= 3:
     print("initial level")
-elif 4 <= grade <= 6:
+
+# If the grade is between 4 and 6 (inclusive)
+elif grade <= 6:
     print("average level")
-elif 7 <= grade <= 9:
+
+# If the grade is between 7 and 9 (inclusive)
+elif grade <= 9:
     print("sufficient level")
-elif 10 <= grade <= 12:
+
+# If the grade is between 10 and 12 (inclusive)
+elif grade <= 12:
     print("high level")
+
+# If the grade is not within the range of 1-12
 else:
     print("level is absent")
