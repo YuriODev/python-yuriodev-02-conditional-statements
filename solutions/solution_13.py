6f7374e89ba25d84bdcfd45e0fc10bc8cd578816
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-21 14:34:52 +0000

    Update solution_13.py

--- solutions/solution_13.py
+++ solutions/solution_13.py
@@ -7,7 +7,7 @@ hundreds = (number % 1000) // 100
 tens = (number % 100) // 10
 ones = number % 10
 
-# Compare digits for uniqueness
+# Compare digits for uniqueness (i.e., no two digits are the same)
 is_unique = (thousands != hundreds or thousands == 0 or hundreds == 0) and \
             (thousands != tens or thousands == 0 or tens == 0) and \
             (thousands != ones or thousands == 0) and \
