2ec34215a9e73cedaf32650025b47bedc3d79d93
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-21 14:32:05 +0000

    Update solution_10.py

--- solutions/solution_10.py
+++ solutions/solution_10.py
@@ -1,13 +1,16 @@
 
+# Prompt the user to enter the coordinates of three points
 x1, y1 = int(input("Enter x1: ")), int(input("Enter y1: "))
 x2, y2 = int(input("Enter x2: ")), int(input("Enter y2: "))
 x3, y3 = int(input("Enter x3: ")), int(input("Enter y3: "))
+
+# Calculate the squares of the distances between the points
 a2 = (x2-x1)**2 + (y2-y1)**2
 b2 = (x3-x2)**2 + (y3-y2)**2
 c2 = (x1-x3)**2 + (y1-y3)**2
 
-# Need to check that either of the sides is equal to 0
-# We can use the Pythagoras theorem to check if the triangle is right-angled
+# Check if any of the sides of the triangle is equal to 0
+# We can use the Pythagorean theorem to check if the triangle is right-angled
 # a2 + b2 == c2 or b2 + c2 == a2 or c2 + a2 == b2
 # If any of these conditions is True, then the triangle is right-angled
 # Otherwise, it is not
