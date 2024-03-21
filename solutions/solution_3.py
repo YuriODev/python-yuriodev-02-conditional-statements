b656b38c00dbb1dd3997fa1ee51d9d75d244a337
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-21 13:57:44 +0000

    Update solution_3.py

--- solutions/solution_3.py
+++ solutions/solution_3.py
@@ -1,12 +1,21 @@
+
+# Prompt the user to enter a number between 0 and 36
 number = int(input("Enter a number (0-36): "))
 
+# Initialize the output variable to "The bet will play!"
 output = "The bet will play!"
 
+# Check the number entered and determine the color of the bet
 if number == 0:
     output = "Green"
+# Check if the number is in the range of 1-10 or 19-28
 elif 1 <= number <= 10 or 19 <= number <= 28:
+    # If the number is odd, the color is Black, otherwise, it's Red
     output = "Black" if number % 2 else "Red"
+# Check if the number is in the range of 11-18 or 29-36
 elif 11 <= number <= 18 or 29 <= number <= 36:
+    # If the number is odd, the color is Red, otherwise, it's Black
     output = "Red" if number % 2 else "Black"
 
+# Print the color of the bet
 print(output)
