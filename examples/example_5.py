661660144d44728fd396308a4a9797907ee68a8d
Yurii Oksamytnyi <141469505+YuriiOks@users.noreply.github.com>
2024-05-23 10:54:18 +0100

    Update example_5.py

--- examples/example_5.py
+++ examples/example_5.py
@@ -1,20 +1,26 @@
 # Input: Student's test score
 score = int(input("Enter the test score: "))
 
-# Initialize an empty string for the output message
-output = ""
 
 # Conditional statements to determine the grade
-if score >= 90:
-    output = "Your grade is A."
-elif score >= 80:
-    output = "Your grade is B."
-elif score >= 70:
-    output = "Your grade is C."
-elif score >= 60:
-    output = "Your grade is D."
-else:
-    output = "Your grade is F."
+if 0 < score <= 100:
+    # Initialize an empty string for the output message
+    output = ""
+    
+    if score >= 90:
+        output = "A"
+    # elif 80 < score < 90:
+    # elif score < 90 and score >= 80:
+    elif score >= 80:
+        output = "B"
+    elif score >= 70:
+        output = "C"
+    elif score >= 60:
+        output = "D"
+    else:
+        output = "F"
 
-# Print the output message
-print(output)
+    # Print the output message
+    print(f"Your grade is {output}.")
+else:
+    print("The entered score is invalid")
